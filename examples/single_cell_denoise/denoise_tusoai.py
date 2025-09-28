318ab9be642924f891d59b02f44509572b3662d4
Alistair-Turcan <18133636+Alistair-Turcan@users.noreply.github.com>
2025-09-28 10:43:18 -0400

    Update denoise_tusoai.py

--- examples/single_cell_denoise/denoise_tusoai.py
+++ examples/single_cell_denoise/denoise_tusoai.py
@@ -11,31 +11,6 @@ from scipy.sparse import issparse
 from sklearn.decomposition import PCA
 from anndata import AnnData
 import random
-
-def mse(adata):
-    import anndata
-    import scanpy as sc
-    import scprep
-    import sklearn.metrics
-
-    test_data = anndata.AnnData(X=adata.obsm["test"], obs=adata.obs, var=adata.var)
-    denoised_data = anndata.AnnData(
-        X=adata.obsm["denoised"], obs=adata.obs, var=adata.var
-    )
-
-    # scaling and transformation
-    target_sum = 10000
-
-    sc.pp.normalize_total(test_data, target_sum=target_sum)
-    sc.pp.log1p(test_data)
-
-    sc.pp.normalize_total(denoised_data, target_sum=target_sum)
-    sc.pp.log1p(denoised_data)
-
-    error = sklearn.metrics.mean_squared_error(
-        scprep.utils.toarray(test_data.X), denoised_data.X
-    )
-    return error
 import numpy as np
 from sklearn.decomposition import NMF
 from scipy.sparse import csr_matrix
@@ -68,15 +43,4 @@ def tuso_model(adata):
     adata.obsm["denoised"] = denoised_data
     
     return adata
-def main():
-    np.random.seed(42)
-    random.seed(42)
-    adata = read_h5ad('openproblems_datasets/1k_pbmc_processed.h5ad')
-    print("tuso_model_start")
-    adata = tuso_model(adata)
-    print("tuso_model_end")
 
-    val_metric = 1-mse(adata)
-    print(f"tuso_evaluate: {val_metric}")
-
-main()
